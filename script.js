452b96af1e03075acaf1a9acb5e9490f391753f5
+ <+>
2022-08-12 14:30:59 -0400

    collors

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Thank you for visiting my first website";
 document.querySelector("body").appendChild(h2);
