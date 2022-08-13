7299fe9534ba4384d0df81ac2872920e38109f9e
+ <+>
2022-08-13 08:22:38 -0400

    add elementById

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Thank you for visiting my first website";
 document.querySelector("body").appendChild(h2);
+
+const div = document.querySelector('.drill_cursor');
+
+div.addEventListener('click', (event) => {
+  console.log('Hi!');
+});
